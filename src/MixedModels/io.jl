b1538cc50148a6bd2f10b598816b25ccdce455b3
Jejulia <54415349+Jejulia@users.noreply.github.com>
2021-09-23 15:24:10 +0800

    Replace collect with vectorize in _anovatable

--- src/MixedModels/io.jl
+++ src/MixedModels/io.jl
@@ -4,7 +4,7 @@ coefnames(model::MixedModel, anova::Val{:anova}) = vectorize(coefnames(model.for
 
 # anovatable api
 function _anovatable(aov::AnovaResult{<: LinearMixedModel, FTest}; kwargs...)
-    AnovaTable(hcat(collect.((
+    AnovaTable(hcat(vectorize.((
                     dof(aov), 
                     dof_residual(aov), 
                     teststat(aov), 
@@ -13,25 +13,13 @@ function _anovatable(aov::AnovaResult{<: LinearMixedModel, FTest}; kwargs...)
               ["DOF", "Res.DOF", "F value", "Pr(>|F|)"],
               ["x$i" for i in eachindex(pval(aov))], 4, 3)
 end
-#=
-function anovatable(stats::MixedAnovaStatsLRT{LinearMixedModel, N}; kwargs...) where N
-    AnovaTable(hcat([stats.dof...], [stats.deviance...], [stats.lrstat...], [stats.pval...]),
-              ["DOF", "Deviance", "Likelihood Ratio", "Pr(>|χ²|)"],
-              ["x$i" for i = 1:length(stats.dof)], 4, 3)
-end 
 
-function anovatable(stats::NestedAnovaStatsF; modeltype::Type{<: LinearMixedModel}, model::Tuple, kwargs...) where N
-    AnovaTable(hcat([stats.dof...], [NaN, _diff(stats.dof)...], stats.nobs .+ 1 .- [stats.dof...], [aic.(model)...], [bic.(model)...], [stats.deviance...], [NaN, _diffn(stats.deviance)...], [stats.fstat...], [stats.pval...]),
-              ["DOF", "ΔDOF", "Res. DOF", "AIC", "BIC", "Deviance", "ΔDeviance", "F value", "Pr(>|F|)"],
-              ["$i" for i = 1:length(stats.dof)], 9, 8)
-end
-=#
 function _anovatable(aov::AnovaResult{<: Tuple, LRT}, 
                     modeltype1::Union{Type{<: LinearMixedModel}, Type{<: TableRegressionModel{<: GLM.LinearModel}}},
                     modeltype2::Type{<: LinearMixedModel}; 
                     kwargs...)
     if last(aov.model).optsum.REML 
-        AnovaTable(hcat(collect.((
+        AnovaTable(hcat(vectorize.((
                         dof(aov), 
                         [NaN, _diff(dof(aov))...], 
                         dof_residual(aov), 
@@ -42,7 +30,7 @@ function _anovatable(aov::AnovaResult{<: Tuple, LRT},
                 ["DOF", "ΔDOF", "Res.DOF", "-2 logLik", "χ²", "Pr(>|χ²|)"],
                 ["$i" for i in eachindex(pval(aov))], 6, 5)
     else
-        AnovaTable(hcat(collect.((
+        AnovaTable(hcat(vectorize.((
                         dof(aov), 
                         [NaN, _diff(dof(aov))...], 
                         dof_residual(aov), 
