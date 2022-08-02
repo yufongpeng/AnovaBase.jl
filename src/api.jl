a73d91041c122f4c58a8cb4c2c72a7f66a4cda16
yufongpeng <54415349+yufongpeng@users.noreply.github.com>
2022-08-03 02:24:23 +0800

    Delete default error methods

--- src/api.jl
+++ src/api.jl
@@ -5,7 +5,6 @@
 
 Unified api for formula of each statistical model.
 """
-formula(model) = error("formula is not defined for $(typeof(model)).")
 formula(trm::TableRegressionModel) = trm.mf.f
 
 """
@@ -23,11 +22,15 @@ coefnames(model::RegressionModel, anova::Val{:anova}) = coefnames(formula(model)
 
 Generate nested models from a model or formula and data.
 """
-nestedmodels(model) = error("nestedmodels is not defined for $(typeof(model)).")
+function nestedmodels end
 
 # implement drop1/add1 in R?
 """
     anova(<models>...; test::Type{<: GoodnessOfFit})
+    anova(test::Type{FTest}, <model>; <keyword arguments>)
+    anova(test::Type{FTest}, <models>...; <keyword arguments>)
+    anova(test::Type{LRT}, <model>; <keyword arguments>)
+    anova(test::Type{LRT}, <models>...; <keyword arguments>)
 
 Analysis of variance.
 
@@ -36,27 +39,7 @@ Return `AnovaResult{M, test, N}`. See [`AnovaResult`](@ref) for details.
 * `models`: model objects. If mutiple models are provided, they should be nested and the last one is the most saturated.
 * `test`: test statistics for goodness of fit. Available tests are [`LikelihoodRatioTest`](@ref) ([`LRT`](@ref)) and [`FTest`](@ref).
 """
-anova(model) = error("anova is not defined for $(typeof(model)).")
-
-"""
-    anova(::Type{FTest}, <model>; <keyword arguments>)
-    anova(::Type{FTest}, <models>...; <keyword arguments>)
-
-Analysis of Variance by F-test.
-
-Return `AnovaResult{M, FTest, N}`. See [`AnovaResult`](@ref) for details.
-"""
-anova(::Type{FTest}, model) = error("anova by F-test is not defined for $(typeof(model)).")
-
-"""
-    anova(::Type{LRT}, <model>; <keyword arguments>)
-    anova(::Type{LRT}, <models>...; <keyword arguments>)
-
-Analysis of Variance by likelihood-ratio test.
-
-Return `AnovaResult{M, LRT, N}`. See [`AnovaResult`](@ref) for details.
-"""
-anova(::Type{LRT}, model) = error("anova by likelihood-ratio test is not defined for $(typeof(model)).")
+function anova end
 
 """
     nobs(aov::AnovaResult{<: Tuple})
@@ -82,8 +65,6 @@ dof(aov::AnovaResult) = aov.dof
 Degree of freedom of residuals.
 
 Default is applying `dof_residual` to models in `aov.model`.
-!!! note 
-    For `MixedModels` applying [`FTest`](@ref), it is calculated by between-within method. See [`calcdof`](@ref) for details.
 """
 dof_residual(aov::AnovaResult{<: Tuple}) = dof_residual.(aov.model)
 dof_residual(aov::AnovaResult) = dof_residual(aov.model)
@@ -92,10 +73,10 @@ dof_residual(aov::AnovaResult) = dof_residual(aov.model)
     deviance(aov::AnovaResult)
 
 Return the stored devaince. The value repressents different statistics for different models and tests.
-1. Ordinary linear regression: sum of squares.
-2. Generalized linear regression`: deviance.
-3. Mixed-effect model: `NaN` when applying [`FTest`](@ref); -2loglikelihood or deviance when applying [`LRT`](@ref).
-When ordinary linear regression is compared to linear mixed-effect model, the deviance is alternatively -2loglikelihood.
+1. Ordinary linear regression: `sum of squares`
+2. Generalized linear regression: `deviance`
+3. Mixed-effect model: `NaN` when applying [`FTest`](@ref); `-2×loglikelihood` or `deviance` when applying [`LRT`](@ref)
+When ordinary linear regression is compared to linear mixed-effect model, the deviance is alternatively `-2×loglikelihood`
 """
 deviance(aov::AnovaResult) = aov.deviance
 
