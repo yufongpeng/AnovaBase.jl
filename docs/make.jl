a1c8f6d3c2bfccf553f2ff66ac7309c3df1e8689
yufongpeng <54415349+yufongpeng@users.noreply.github.com>
2023-03-22 16:07:25 +0800

    Update doc

--- docs/make.jl
+++ docs/make.jl
@@ -1,50 +1,6 @@
 push!(LOAD_PATH,"../src/")
 using Documenter
 using AnovaBase, GLM, AnovaGLM, AnovaMixedModels, AnovaFixedEffectModels, DataFrames
-import AnovaBase: _diff, teststat, AnovaTable, pval
-import AnovaMixedModels: GLM_MODEL, anovatable, AnovaResult, LRT, NestedModels
-#DocMeta.setdocmeta!(AnovaBase, :DocTestSetup, :(using AnovaBase); recursive = true)
-teststat(aov::AnovaResult) = aov.teststat
-pval(aov::AnovaResult) = aov.pval
-function anovatable(aov::AnovaResult{NestedModels{M, N}, LRT}; 
-                    rownames = string.(1:N)) where {M <: Union{GLM_MODEL, MixedModel}, N}
-    if last(aov.anovamodel.model) isa GLM_MODEL
-        AnovaTable([
-                dof(aov), 
-                [NaN, _diff(dof(aov))...], 
-                dof_residual(aov), 
-                deviance(aov), 
-                teststat(aov), 
-                pval(aov)
-            ],
-            ["DOF", "ΔDOF", "Res.DOF", "Deviance", "χ²", "Pr(>|χ²|)"],
-            rownames, 6, 5)
-    elseif last(aov.anovamodel.model).optsum.REML 
-        AnovaTable([
-                        dof(aov), 
-                        [NaN, _diff(dof(aov))...], 
-                        dof_residual(aov), 
-                        deviance(aov), 
-                        teststat(aov), 
-                        pval(aov)
-                    ],
-                ["DOF", "ΔDOF", "Res.DOF", "-2 logLik", "χ²", "Pr(>|χ²|)"],
-                rownames, 6, 5)
-    else
-        AnovaTable([
-                        dof(aov), 
-                        [NaN, _diff(dof(aov))...], 
-                        dof_residual(aov), 
-                        aic.(aov.anovamodel.model),
-                        bic.(aov.anovamodel.model),
-                        deviance(aov), 
-                        teststat(aov), 
-                        pval(aov)
-                    ],
-                ["DOF", "ΔDOF", "Res.DOF", "AIC", "BIC", "-2 logLik", "χ²", "Pr(>|χ²|)"],
-                rownames, 8, 7)
-    end
-end
 
 makedocs(
     sitename = "AnovaBase",
@@ -54,6 +10,9 @@ makedocs(
             "Examples_GLM.md",
             "Examples_MixedModels.md",
             "Examples_FixedEffectModels.md"
+        ],
+        "Interfacing AnovaBase.jl" => [
+            "Interface.md"
         ],
         "Algorithm" => [
             "Algorithm_AnovaGLM.md",
